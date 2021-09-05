5ca62b4e34b16c1802484b3f10b63cf89a3f5d11
Zach Rickers <mzrickers@gmail.com>
2021-09-05 18:17:26 +0200

    navigation bar on top and left

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
         <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
       </div>
       <div class="nav-links">
-        <ul>
+        <ul v-show="!mobile">
           <router-link class="link" to="#">Home</router-link>
           <router-link class="link" to="#">Blogs</router-link>
           <router-link class="link" to="#">Create Post</router-link>
@@ -13,9 +13,9 @@
         </ul>
       </div>
     </nav>
-    <menuIcon class="menu-icon"/>
+    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
     <transition name="mobile-nav">
-      <ul>
+      <ul class="mobile-nav" v-show="mobileNav">
         <router-link class="link" to="#">Home</router-link>
         <router-link class="link" to="#">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
@@ -31,6 +31,33 @@ export default {
   name: 'navigation',
   components: {
     menuIcon
+  },
+  data () {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    }
+  },
+  created() {
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
+    },
+    
+    toggleMobileNav () {
+      this.mobileNav = !this.mobileNav;
+    }
   }
 }
 </script>
@@ -67,6 +94,54 @@ nav {
       color: #000;
       text-decoration: none;
     }
+  }
+
+  .nav-links {
+    position: relative;
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: flex-end;
+
+    ul {
+      margin-right: 32px;
+
+      .link {
+        margin-right: 32px;
+      }
+
+      .link:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+
+.menu-icon {
+  cursor: pointer;
+  position: absolute;
+  top: 32px;
+  right: 25px;
+  height: 25px;
+  width: auto; 
+}
+
+.mobile-nav {
+  padding: 20px;
+  width: 70%;
+  max-width: 250px;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  height: 100%;
+  background-color: #303030;
+  top: 0;
+  left: 0;
+
+  .link {
+    padding: 15px 0;
+    color: #fff;
+
   }
 }
 </style>
